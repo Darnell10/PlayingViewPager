68f23de2830a1bd7b2adb8c1a20ad325c9bfb23a
Darnell O <DarnellOtto@ac.c4q.nyc>
2019-03-19 17:00:12 -0400

    add gradle dependancies

--- app/src/main/java/com/example/viewpagerexample/ViewPagerFragment.java
+++ app/src/main/java/com/example/viewpagerexample/ViewPagerFragment.java
@@ -2,10 +2,14 @@ package com.example.viewpagerexample;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +17,54 @@ import android.view.ViewGroup;
  */
 public class ViewPagerFragment extends Fragment {
 
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARMA2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private TextView textView;
+    private ImageView imageView;
+
 
     public ViewPagerFragment() {
         // Required empty public constructor
     }
 
+    public static ViewPagerFragment newInstance(String param1, String param2){
+        ViewPagerFragment viewPagerFragment = new ViewPagerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1,param1);
+        args.putString(ARG_PARMA2,param2);
+        viewPagerFragment.setArguments(args);
+        return viewPagerFragment;
+    }
+
+    //gets data when view is created
+    @Override
+    public void onCreate(Bundle savedInstanceBundle){
+        super.onCreate(savedInstanceBundle);
+        if (getArguments() != null){
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2= getArguments().getString(ARG_PARMA2);
+        }
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_pager, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        textView = view.findViewById(R.id.viewpager_textView);
+        imageView= view.findViewById(R.id.viewpager_imageview);
+        textView.setText(mParam1);
+
     }
 
 }
